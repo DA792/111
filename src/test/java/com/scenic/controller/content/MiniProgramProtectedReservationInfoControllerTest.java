63765382e499e3531b1b6b2444d5d33cbd86c7c3
yuzhou.lin <yuzhou.lin@npctek.com>
2025-10-21 17:01:08 +0800

    修复合并错误

--- src/test/java/com/scenic/controller/content/MiniProgramProtectedReservationInfoControllerTest.java
+++ src/test/java/com/scenic/controller/content/MiniProgramProtectedReservationInfoControllerTest.java
@@ -2,14 +2,11 @@ package com.scenic.controller.content;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.runner.RunWith;
 
 /**
  * 小程序端保护区介绍控制器测试
  */
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class MiniProgramProtectedReservationInfoControllerTest {
     
     @Test
