3924d959762964f9a03371ff37e392f5833495d6
xuan.yang <xuan.yang@npctek.com>
2025-10-28 14:10:54 +0800

    fix:修复新增接口后端无法启动的问题

--- src/main/java/com/scenic/service/system/impl/ThemeSettingServiceImpl.java
+++ src/main/java/com/scenic/service/system/impl/ThemeSettingServiceImpl.java
@@ -1,12 +1,17 @@
 package com.scenic.service.system.impl;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.scenic.common.dto.Result;
 import com.scenic.dto.system.ThemeSettingDTO;
